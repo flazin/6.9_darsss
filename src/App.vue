e3ccd13a84da7e74158e104544a24be4bff8699e
akmaljon <akmaljon@gmail.com>
2024-02-25 18:02:31 +0500

    DSFE

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ let open = ref(false);
 </script>
 
 <template>
-  <header class="bg-indigo-500">
+  <header class="bg-indigo-800">
     <nav
       class="container relative flex items-center justify-between px-6 py-8 mx-auto text-white"
     >
