d6ad484f0952a163e111ec6ffd56987560006b02
akmaljon <akmaljon@gmail.com>
2024-02-25 18:01:09 +0500

    daWD

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ let open = ref(false);
 </script>
 
 <template>
-  <header class="background-color:blue;">
+  <header class="bg-indigo-500">
     <nav
       class="container relative flex items-center justify-between px-6 py-8 mx-auto text-white"
     >
