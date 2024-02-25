fbb0338559f7fab93be8a39dbb3e15aa978bb006
akmaljon <akmaljon@gmail.com>
2024-02-25 16:22:44 +0500

    chanegd

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,11 @@ import { ref } from "vue";
 let open = ref(false);
 </script>
 
-<template class="body">
+<template>
   <header class="bg-indigo-500">
     <nav
       class="container relative flex items-center justify-between px-6 py-8 mx-auto text-white"
     >
-      <a href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-8 h-8 md:w-10 md:h-10"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
-          />
-        </svg>
-      </a>
-
       <button @click="open = !open" class="md:hidden">
         <span v-show="!open">
           <svg
@@ -64,6 +47,7 @@ let open = ref(false);
         :class="[
           open ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full',
         ]"
+        class="absolute inset-x-0 z-30 w-full px-6 py-8 mt-4 space-y-6 transition-all duration-300 ease-in-out bg-indigo-600 top-16 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:space-y-0 md:-mx-6 md:flex md:items-center"
       >
         <a
           href="#"
@@ -104,76 +88,21 @@ let open = ref(false);
         class="relative grid w-full bg-indigo-500 h-96 lg:h-[32rem] place-items-center"
       >
         <div class="flex flex-col items-center mx-auto text-center">
-          <h1 class="text-2xl font-semibold text-white uppercase md:text-3xl">
-            Hi , I’m Charles, Front-end Developer
+          <h1 class="title">
+            Hi , I’m Charles, <br /><br /><br /><br /><br />
+            Front-end Developer
           </h1>
 
-          <p class="mt-6 text-lg leading-5 text-white">
-            I design and develop experiences that make people’s lives simpler
-            through Web and Mobile apps.I work with FIgma , HTML5, CSS3,
-            JavaScript, React, ReactNative and Flutter.
+          <p class="desc">
+            I design and develop experiences that make people’s lives
+            <br /><br /><br /><br /><br /><br /><br />
+            simpler through Web and Mobile apps.I work with FIgma ,
+            <br /><br /><br /><br /><br /><br /><br />
+            HTML5, CSS3, JavaScript, React, ReactNative and Flutter.
           </p>
 
           <button class="here">HERE ME</button>
           <button class="hare">SEE MY PROJECTS</button>
-          <a href="#about" class="mt-8 cursor-pointer animate-bounce"> </a>
-        </div>
-      </div>
-
-      <svg
-        class="fill-indigo-500"
-        viewBox="0 0 1440 57"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M1440 0H0V57C720 0 1440 57 1440 57V0Z" />
-      </svg>
-    </section>
-
-    <section class="container px-6 py-8 mx-auto lg:py-16" id="about">
-      <div class="lg:flex lg:items-center lg:-mx-4">
-        <div class="lg:w-1/2 lg:px-4">
-          <h3 class="text-xl font-medium text-gray-800 md:text-2xl lg:text-3xl">
-            We create awesome prototypes, plugins, and templates.
-          </h3>
-
-          <p class="mt-6 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-            laboriosam provident voluptatum id magni iste nobis corrupti,
-            delectus quis repellat, debitis error quod explicabo molestiae rerum
-            totam ab sunt excepturi?
-          </p>
-
-          <button class="flex items-center mt-8 -mx-2 text-indigo-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-8 h-8 mx-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <p class="mx-1 font-semibold">PLAY VIDEO</p>
-          </button>
-        </div>
-
-        <div class="mt-8 lg:w-1/2 lg:px-4 lg:mt-0">
-          <img
-            class="object-cover w-full rounded-xl h-96"
-            src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            alt="Video thumbnail"
-          />
         </div>
       </div>
     </section>
